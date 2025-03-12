06882178f3d2c2600260970ad78adf43b35c3c28
Thomas Kristensen <thomas@sl.m04r.space>
2025-03-12 19:00:50 +0100

    ci: update renovate configuration

--- renovate-config.js
+++ renovate-config.js
@@ -6,5 +6,19 @@ module.exports = {
     ":enablePreCommit"
   ],
   labels: ["dependencies"],
+  ansible: {
+    fileMatch: [
+      "(^|/)tasks/[^/]+\\.ya?ml$"
+    ]
+  },
+  lockFileMaintenance: {
+    enabled: true
+  },
+  automerge: true,
+  ignoreTests: false,
+  configMigration: true,
   repositories: [{"repository": "tkk2112/dotfiles"}]
+  "vulnerabilityAlerts": {
+    "enabled": true
+  }
 };
