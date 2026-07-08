004000c784f600a81499771beb507b7f144a65fd
Thomas Kristensen <thomas@sl.m04r.space>
2026-07-08 10:37:20 +0200

    Reduce setup bootstrap log noise

--- setup.sh
+++ setup.sh
@@ -28,9 +28,11 @@ if ! command -v chezmoi >/dev/null 2>&1; then
     mkdir -p "$bin_dir"
 
     if command -v curl >/dev/null 2>&1; then
-        run sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir"
+        log "Installing chezmoi with curl into $bin_dir"
+        sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir"
     elif command -v wget >/dev/null 2>&1; then
-        run sh -c "$(wget -qO- get.chezmoi.io)" -- -b "$bin_dir"
+        log "Installing chezmoi with wget into $bin_dir"
+        sh -c "$(wget -qO- get.chezmoi.io)" -- -b "$bin_dir"
     else
         printf "chezmoi is not installed and neither curl nor wget is available.\n" >&2
         exit 1
